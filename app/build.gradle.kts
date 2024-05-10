a14efe77660ae5d63ffeee589e013ad9c847207b
Dineth Chamupathi Kumara <127776710+ChamupathiDev@users.noreply.github.com>
2024-05-10 18:45:11 +0530

    tt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id ("com.google.devtools.ksp")
+
 }
 
 android {
@@ -33,6 +36,10 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    buildFeatures {
+        viewBinding  = true
+    }
 }
 
 dependencies {
@@ -44,4 +51,34 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+
+    //Lottie
+    implementation ("com.airbnb.android:lottie:6.1.0")
+
+    //Room
+    val roomVersion = "2.6.1"
+
+    implementation ("androidx.room:room-runtime:$roomVersion")
+
+    implementation ("androidx.room:room-ktx:$roomVersion")
+
+
+    ksp ("androidx.room:room-compiler:$roomVersion")
+
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+
+
+
+
+
+
+
+
 }
+
+
